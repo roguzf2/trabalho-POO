85c844ccd84e3ed2b5bdcfeef59cc82304df0954
Rafael <95830415+Rafahgol@users.noreply.github.com>
2023-06-14 12:08:05 -0300

    PostFosto atributos e metodos, Comentario modifica do

--- Comentario.java
+++ Comentario.java
@@ -5,4 +5,36 @@ public class Comentario{
     private boolean fixado;
     private int tamanho;
     private String texto;
+    public Object data_postagem; // adicionado automaticamente
+    
+    public Comentario(int tamanho, String texto) {
+        this.data = data;
+        this.fixado = fixado;
+        this.tamanho = tamanho;
+        this.texto = texto;
+    }
+    public LocalDateTime getData() {
+        return data;
+    }
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+    public boolean isFixado() {
+        return fixado;
+    }
+    public void setFixado(boolean fixado) {
+        this.fixado = fixado;
+    }
+    public int getTamanho() {
+        return tamanho;
+    }
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+    public String getTexto() {
+        return texto;
+    }
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 }
--- PostFoto.java
+++ PostFoto.java
@@ -1,27 +1,65 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Date;
 
 public  class PostFoto{
-    private int qtde_fotos;
-    private Foto fotos[];
+    private int qtde_fotos, tamanho_antes;
+    private ArrayList<Foto> fotos;
     private String localizacao;
     private LocalDateTime data_postagem;
-    private Comentario listaComentarios[];
+    private ArrayList<Comentario> listaComentarios;
     
-    public void PostFoto(){
-
+    public PostFoto(){
+        
     }
 
-    public boolean adicionaFoto(){
-        return true;
-    }
+    public boolean adicionaFoto(Foto foto){
+        tamanho_antes = fotos.size();
+        fotos.add(foto);
+        qtde_fotos += 1;
+        if ((fotos.size()) > 10){
+            System.out.println("Erro! Limite maximo de fotos excedido.");
+            return false;
+        }
+        if (tamanho_antes == fotos.size()){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }   
 
-    public boolean removeFoto(){
-        return false;
+    public boolean removeFoto(Foto foto){
+        tamanho_antes = fotos.size();
+        fotos.remove(foto);
+        if (fotos.size() == 0){
+            System.out.println("Erro! Não há fotos a serem removidas.");
+            return false;
+        }
+        if (tamanho_antes > fotos.size()){
+            return true;
+        }
+        else{
+            System.out.println("Erro! Foto não foi removida.");
+            return false;
+        }
     }
     public boolean posta(){
-        return true;
+        if (fotos.size()> 1 && fotos.size() <=10)
+        {
+            this.data_postagem = data_postagem;
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    public boolean comenta(){
-        return true;
+    public boolean comenta(int new_tamanho, String new_texto){
+        Comentario comentario = new Comentario(new_tamanho, new_texto); 
+        comentario.setData(data_postagem);
+        listaComentarios.add(comentario);
+        return true;    
     }
 }
