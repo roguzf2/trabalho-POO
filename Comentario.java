683738028be6ca191548ccb0debe7cf65b0c3478
Rafael <95830415+Rafahgol@users.noreply.github.com>
2023-06-14 10:39:18 -0300

    Atributos e metodos de Comentario, PostavelFactory , PostFoto, PostVideo.

--- Comentario.java
+++ Comentario.java
@@ -1,3 +1,8 @@
+import java.time.LocalDateTime;
+
 public class Comentario{
-    
+    private LocalDateTime data;
+    private boolean fixado;
+    private int tamanho;
+    private String texto;
 }
